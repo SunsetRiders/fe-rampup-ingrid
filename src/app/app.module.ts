3c02a9bd8a9b7a7c24d9ff6cd26d20c90992b170
Françoise Ingrid Vieira Martins <fmartins@brdlfmartins.daitan.com>
2018-09-28 13:59:49 -0300

    style(card): create professional skills

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { ProfileComponent } from './profile/profile.component';
 import { DownloadResumeComponent } from './download-resume/download-resume.component';
+import { ProfessionalSkillsComponent } from './professional-skills/professional-skills.component';
+import { OurServicesComponent } from './our-services/our-services.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
     ProfileComponent,
-    DownloadResumeComponent
+    DownloadResumeComponent,
+    ProfessionalSkillsComponent,
+    OurServicesComponent
   ],
   imports: [
     BrowserModule
