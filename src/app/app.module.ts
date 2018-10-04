0c779ce513cb5ed4bbc035276bf29401cca104b4
Françoise Ingrid Vieira Martins <fmartins@brdlfmartins.daitan.com>
2018-10-04 16:04:58 -0300

    refactor(profile.scss): refactor style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { CardComponent } from './card/card.component';
 import { ProfileComponent } from './profile/profile.component';
 import { DownloadResumeComponent } from './download-resume/download-resume.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
     CardComponent,
     ProfileComponent,
     DownloadResumeComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
