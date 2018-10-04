d236eeaf4658c81f8d1fb85879f2b89b582f4c39
Françoise Ingrid Vieira Martins <fmartins@brdlfmartins.daitan.com>
2018-10-04 16:14:38 -0300

    refactor(app): remove some files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CardComponent } from './card/card.component';
 import { ProfileComponent } from './profile/profile.component';
-import { DownloadResumeComponent } from './download-resume/download-resume.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent,
     ProfileComponent,
-    DownloadResumeComponent,
     AboutComponent,
   ],
   imports: [
