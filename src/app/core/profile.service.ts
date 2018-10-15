7d0e3b50de30f2cb4f63a0f3cbb5c8ddd3672367
Françoise Ingrid Vieira Martins <fmartins@brdlfmartins.daitan.com>
2018-10-15 16:00:35 -0300

    refactor(contact me): test functions

--- src/app/core/profile.service.ts
+++ src/app/core/profile.service.ts
@@ -11,9 +11,27 @@ export class ProfileService {
                   {name: 'Skype', content: 'robsmithcompany'},
                   {name: 'Adress', content: '24058, Belgium, Brussels, Liutte 27, BE'}];
 
+  private teste = [{birthday: '1996-12-21T02:00:00.000Z' ,
+                   email: 'robertsmith@company.com',
+                   Phone: '+1 256 243 84 56',
+                   Viber: '+1 256 243 84 56',
+                   Skype: 'robsmithcompany',
+                   Adress: '24058, Belgium, Brussels, Liutte 27, BE'}];
+
+  private birthdayy = '1996-12-21T02:00:00.000Z';
+
   constructor() { }
 
   get userInfo() {
     return this.info;
   }
+
+  get userTeste() {
+    return this.teste;
+  }
+
+  get birthday() {
+    return this.birthdayy;
+  }
+
 }
