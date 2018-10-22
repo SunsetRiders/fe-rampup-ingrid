37fe5e0bddfaad8c4d981783981f60f47f0b6b69
Françoise Ingrid Vieira Martins <fmartins@brdlfmartins.daitan.com>
2018-10-22 16:48:20 -0300

    refactor(app): remove code in app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -8,10 +9,11 @@ import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { GuardService } from './core/services/guard/guard.service';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 
 @NgModule({
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, NavBarComponent ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -20,7 +22,7 @@ import { GuardService } from './core/services/guard/guard.service';
     SharedModule,
     ReactiveFormsModule
   ],
-  providers: [ GuardService],
+  providers: [ GuardService ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
