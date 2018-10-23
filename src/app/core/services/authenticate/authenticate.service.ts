c3efd8aa771c616ab282b964686be54a0327e43b
Françoise Ingrid Vieira Martins <fmartins@brdlfmartins.daitan.com>
2018-10-23 15:26:23 -0300

    refactor(authenticate): nav route

--- src/app/core/services/authenticate/authenticate.service.ts
+++ src/app/core/services/authenticate/authenticate.service.ts
@@ -1,21 +1,24 @@
 import { Injectable } from '@angular/core';
 import { GuardService } from '../guard/guard.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticateService {
   private authenticated = true;
-  constructor () {}
+  constructor (private router: Router) {}
 
   changeTrue() {
     this.authenticated = true;
     console.log(this.authenticated);
+    this.router.navigate(['/skill']);
   }
 
   changeFalse() {
     this.authenticated = false;
     console.log(this.authenticated);
+    this.router.navigate(['/page-not-found']);
   }
 
   getStatus(): boolean {
