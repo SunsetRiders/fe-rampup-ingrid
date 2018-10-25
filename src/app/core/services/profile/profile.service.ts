6b5065554387acfbfe61e3a348ef5d5bfe9c401a
Françoise Ingrid Vieira Martins <fmartins@brdlfmartins.daitan.com>
2018-10-25 16:36:04 -0300

    feat(resolve): exuperio did help

--- src/app/core/services/profile/profile.service.ts
+++ src/app/core/services/profile/profile.service.ts
@@ -1,53 +1,53 @@
-import { Injectable } from "@angular/core";
-import { ProfileInfo } from "./profile-info";
-import { ProfileInfoType } from "./profile-info-type";
-import { of, Observable } from "rxjs";
-import { HttpClient } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { ProfileInfo } from './profile-info';
+import { ProfileInfoType } from './profile-info-type';
+import { of, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class ProfileService {
   private info: ProfileInfo[] = [
     {
-      name: "E-mail",
-      content: "robertsmith@company.com",
+      name: 'E-mail',
+      content: 'robertsmith@company.com',
       type: ProfileInfoType.Default,
       highlight: true
     },
     {
-      name: "Birthday",
-      content: "1996-12-21T02:00:00.000Z",
+      name: 'Birthday',
+      content: '1996-12-21T02:00:00.000Z',
       type: ProfileInfoType.Age,
       highlight: false
     },
     {
-      name: "Phone",
-      content: "+1 256 243 84 56",
+      name: 'Phone',
+      content: '+1 256 243 84 56',
       type: ProfileInfoType.Default,
       highlight: false
     },
     {
-      name: "Viber",
-      content: "+1 256 243 84 56",
+      name: 'Viber',
+      content: '+1 256 243 84 56',
       type: ProfileInfoType.Default,
       highlight: false
     },
     {
-      name: "Skype",
-      content: "robsmithcompany",
+      name: 'Skype',
+      content: 'robsmithcompany',
       type: ProfileInfoType.Default,
       highlight: false
     },
     {
-      name: "Adress",
-      content: "24058, Belgium, Brussels, Liutte 27, BE",
+      name: 'Adress',
+      content: '24058, Belgium, Brussels, Liutte 27, BE',
       type: ProfileInfoType.Default,
       highlight: false
     },
     {
-      name: "Vacation",
-      content: "till March 25,2018",
+      name: 'Vacation',
+      content: 'till March 25,2018',
       type: ProfileInfoType.Calendar,
       highlight: true
     }
