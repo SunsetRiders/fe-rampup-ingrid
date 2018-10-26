9ce19defcb045a201e533709365d80c5e653bc31
Françoise Ingrid Vieira Martins <fmartins@brdlfmartins.daitan.com>
2018-10-26 13:11:32 -0300

    refactor(profile service): removed the obj

--- src/app/core/services/profile/profile.service.ts
+++ src/app/core/services/profile/profile.service.ts
@@ -8,56 +8,8 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ProfileService {
-  private info: ProfileInfo[] = [
-    {
-      name: 'E-mail',
-      content: 'robertsmith@company.com',
-      type: ProfileInfoType.Default,
-      highlight: true
-    },
-    {
-      name: 'Birthday',
-      content: '1996-12-21T02:00:00.000Z',
-      type: ProfileInfoType.Age,
-      highlight: false
-    },
-    {
-      name: 'Phone',
-      content: '+1 256 243 84 56',
-      type: ProfileInfoType.Default,
-      highlight: false
-    },
-    {
-      name: 'Viber',
-      content: '+1 256 243 84 56',
-      type: ProfileInfoType.Default,
-      highlight: false
-    },
-    {
-      name: 'Skype',
-      content: 'robsmithcompany',
-      type: ProfileInfoType.Default,
-      highlight: false
-    },
-    {
-      name: 'Adress',
-      content: '24058, Belgium, Brussels, Liutte 27, BE',
-      type: ProfileInfoType.Default,
-      highlight: false
-    },
-    {
-      name: 'Vacation',
-      content: 'till March 25,2018',
-      type: ProfileInfoType.Calendar,
-      highlight: true
-    }
-  ];
 
   constructor(private http: HttpClient) {}
-
-  get userInfo(): ProfileInfo[] {
-    return this.info;
-  }
 
   getUserInfoFromBackEnd(): Observable<ProfileInfo[]> {
     const url = 'http://localhost:3000/profile';
