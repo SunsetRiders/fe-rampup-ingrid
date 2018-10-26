d2bd33babfea355a5aee96bde15afe9cb0f6b36f
Françoise Ingrid Vieira Martins <fmartins@brdlfmartins.daitan.com>
2018-10-26 16:05:02 -0300

    refactor(contact me service): created a post to form

--- src/app/core/services/contact-me-form/contact-me.service.ts
+++ src/app/core/services/contact-me-form/contact-me.service.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@angular/core';
 import { ContactMeForm } from './contact-me-form';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactMeService {
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
 
   sendContent (formInfo: ContactMeForm) {
     console.log(formInfo);
+
+    return console.log(this.http.post('http://localhost:3000/contact', formInfo)
+    .subscribe(
+      data => {
+        console.log('Post Request is successful ', data);
+      },
+      error => {
+        console.log('Error', error);
+      }
+    ));
   }
 
 }
